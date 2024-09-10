7bd82f4e70a6942dc2e4917fe6cadd711fb6d6f0
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-09-10 16:29:26 +0300

    Finish Index Page

---
+++ Register.php
@@ -0,0 +1,56 @@
+<?php require_once "inc/header.php" ?>
+
+<div class="container">
+  <div class="row">
+    <!-- Section: Design Block -->
+    <section class="text-center">
+      <div class="card mx-4 mx-md-5 my-5 shadow-5-strong">
+        <div class="card-body py-5 px-md-5">
+          <div class="row d-flex justify-content-center">
+            <div class="col-lg-8">
+              <h2 class="fw-bold mb-5">Sign up</h2>
+              <?php    
+                if($session->hasSession("errors")){
+                    $errors = $session->Get("errors");
+                    foreach($errors as $error){
+                        echo "<div class='alert alert-danger'>$error</div>";
+                    }
+                    $session->Clear("errors");
+                }
+              ?>
+              <form action="handlers/handleRegister.php" method="POST">
+                <!-- Username input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                  <label class="form-label" for="form3Example1">User Name</label>
+                  <input type="text" id="form3Example1" class="form-control" name="username"/>
+                </div>
+                <!-- Email input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                  <label class="form-label" for="form3Example2">Email address</label>
+                  <input type="text" id="form3Example2" class="form-control" name="email" />
+                </div>
+                <!-- Password input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                  <label class="form-label" for="form3Example3">Password</label>
+                  <input type="password" id="form3Example3" class="form-control" name="password"/>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    I have My Account <a href="./Login.php">Login</a>
+                  </div>
+                  <div class="col-md-6">
+                    <!-- Submit button -->
+                    <button type="submit" name="submit" data-mdb-ripple-init class="btn btn-primary btn-block mb-4">
+                      Sign up
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Section: Design Block -->
+  </div>
+</div>
--- index.php
+++ index.php
@@ -2,48 +2,25 @@
 
 <div class="container my-5">
     <div class="row">
+        <?php 
+        $posts = $db->ReadAll("posts");
+        foreach($posts as $post){
+        ?>
         <div class="col-lg-4 mb-3">
             <div class="card">
-                <img src="images/one.jpg" class="card-img-top">
+                <img src="assets/images/<?= $post['image'] ?>" class="card-img-top">
                 <div class="card-body">
-                    <h5 class="card-title">product 1</h5>
-                    <p class="text-muted">2000 EGP</p>
-                    <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <a href="show.php" class="btn btn-primary">Show</a>
+                    <h5 class="card-title"><?= $post['title'] ?></h5>
+                    <p class="card-text"><?= $Str->excerpt($post['body'], 70); ?></p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <p class="text-muted"><?= $post['created_at'] ?></p>
+                        <a href="show.php?id=<?= $post['id']; ?>" class="btn btn-primary">Show</a>
+                    </div>
 
-                    <a href="edit.php" class="btn btn-info">Edit</a>
-                    <a href="" class="btn btn-danger">Delete</a>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 mb-3">
-            <div class="card">
-                <img src="images/two.jpg" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title">product 2</h5>
-                    <p class="text-muted">2000 EGP</p>
-                    <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-                    <a href="show.php" class="btn btn-primary">Show</a>
-
-                    <a href="edit.php" class="btn btn-info">Edit</a>
-                    <a href="" class="btn btn-danger">Delete</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-            <div class="card">
-                <img src="images/three.jpg" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title">product 3</h5>
-                    <p class="text-muted">2000 EGP</p>
-                    <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <a href="show.php" class="btn btn-primary">Show</a>
-
-                    <a href="edit.php" class="btn btn-info">Edit</a>
-                    <a href="" class="btn btn-danger">Delete</a>
-                </div>
-            </div>
-        </div>        
+        <?php } ?>       
     </div>
 </div>
 
