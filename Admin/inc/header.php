865afbfc3525a96f76cc7e613c3fb0a87247b258
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-09-11 10:27:49 +0300

    Finish User Side

--- Admin/inc/header.php
+++ Admin/inc/header.php
@@ -1,29 +1,43 @@
+<?php require_once "./App.php"; ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BLOG</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">BLOG</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">All Products</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="Add.php">Add Product</a>
-                </li>
-            </ul>
-
-        </div>
-    </div>
-</nav>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BLOG</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">BLOG</a>               
+                <nav class="navbar-nav ml-auto">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <?php
+                            if($Session->hasSession("current_login")){
+                                $current_user = $Session->Get("current_login");
+                                $user_id = $current_user['id'];
+                                $username = $current_user['username'];
+                                $email = $current_user['email'];
+                        ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?id=<?= $user_id; ?>"><?= $username ?></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="handlers/handleLogout.php">Logout</a>
+                        </li>
+                        <?php }else{ ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Signup</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <?php }?>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
--- Admin/index.php
+++ Admin/index.php
@@ -1,3 +1,26 @@
-<?php require_once "inc/header.php";
+<?php require_once "inc/header.php"; ?>
 
+<div class="container my-5">
+    <div class="row">
+        <?php 
+        $posts = $db->ReadAll("posts");
+        foreach($posts as $post){
+        ?>
+        <div class="col-lg-4 mb-3">
+            <div class="card">
+                <img src="assets/images/<?= $post['image'] ?>" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $post['title'] ?></h5>
+                    <small class="text-muted m-0 p-0"><?= $post['created_at'] ?></small>
+                    <p class="card-text"><?= $Str->excerpt($post['body'], 70); ?></p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <a href="ViewPost.php?id=<?= $post['id']; ?>" class="btn btn-primary">View Post</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php } ?>       
+    </div>
+</div>
 
+<?php include 'inc/footer.php'; ?>
